f8ede54da139d8e0ca591e52bb5a1cff204d152b
HarshitBhalani <harshitbhalani187@gmail.com>
2025-07-21 01:41:29 +0530

    Update code and fix environment/config issues

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,4 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const uri = process.env.ATLAS_URI || "";
 const client = new MongoClient(uri, {
